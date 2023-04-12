19521d190d74109bf1e1e5dd1aea524e659b426d
Shinmera <shinmera@tymoon.eu>
2023-04-12 10:29:35 +0200

    Add iterations to digest wrappers

--- digests.lisp
+++ digests.lisp
@@ -7,17 +7,20 @@
 (in-package #:org.shirakumo.crypto-shortcuts)
 
 (defmacro define-digest-wrapper (digest)
-  `(defun ,(intern (string digest)) (string &key (to :hex) encode)
+  `(defun ,(intern (string digest)) (string &key (to :hex) encode (iterations 1))
      ,@(if (ironclad:digest-supported-p (find-symbol (string digest) "IRONCLAD"))
            `(,(format NIL "Turn a string into a ~a-hash.
 
 TO is the returned representation
 ENCODE is the encoding before hashing
+ITERATIONS is the number of times to hash
 
 See TO." digest)
-             (let ((octets (ironclad:digest-sequence
-                            ',digest (to-octets (to encode string)))))
-               (to to octets)))
+             (let ((digest (ironclad:make-digest ',digest)))
+               (ironclad:update-digest digest (to-octets (to encode string)))
+               (dotimes (x (1- iterations))
+                 (ironclad:update-digest digest (ironclad:produce-digest digest)))
+               (to to (ironclad:produce-digest digest))))
            `("Unsupported!"
              (error ,(format NIL "The ~a digest is not supported on your platform!" digest))))))
 
